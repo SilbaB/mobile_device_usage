c7da425772ecc0ec3982b16ef80e951ca04e611a
SilbaB <murkomenbasil@gmail.com>
2024-10-31 22:46:22 +0300

    second commmit

--- app.py
+++ app.py
@@ -1,27 +1,45 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 import joblib
 import numpy as np
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
-# Load the saved model
-model = joblib.load('svm_model.pkl')
+# Route to display the HTML form
+@app.route('/')
+def index():
+    return render_template('form.html')
 
-# Define a route for predictions
-# @app.route('/predict', methods=['POST'])
+# Route to handle form submission
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Retrieve data from form
+    name = request.form.get('name')
+    age = request.form.get('age')
+    email = request.form.get('email')
+    
+    # Process or display the data as needed
+    return f"Hello, {name}! You are {age} years old, and your email is {email}."
+
 @app.route('/')
 def home():
     return "Welcome to the SVM Model API!,its me again, its getting tough,man"
 
 @app.route('/about/<username>')
 def about_page(username):
-    return f'<h1>Hello {username}!</h1>'
+    return f'<h1>Hello {username}!</h1>,<h3> How is the going, I LOVE YOU</h3?'
 
+ 
+model = joblib.load("svm_model.pkl")
 @app.route('/predict', methods=['POST'])
 
 def predict():
     # Get the JSON data from the request
-    data = request.json
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No JSON data provided'}), 400
+    
     features = np.array(data['selected_features_list']).reshape(1, -1)
     
     # Predict the class and probabilities
@@ -34,6 +52,9 @@ def predict():
         'probabilities': probabilities.tolist()
     })
 
+
+    
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run( debug=True)
 
